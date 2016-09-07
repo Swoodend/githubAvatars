fb0066573a09a0cb7fc3a82751aff4731e230c3d
Scott Woodend <woodendscott@gmail.com>
2016-09-07 17:08:17 -0400

    add functionality to stream images to directory, and handle a missing directory

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var fs = require('fs');
 
+
 var username = process.argv[2];
 var repo = process.argv[3];
 
@@ -24,26 +25,30 @@ function getURLArray(obj){
   for (var i = 0; i < obj.length; i++){
     URLS.push(obj[i].avatar_url);
   }
-  streamToDir(URLS);
+  streamToDir(URLS, options.url);
 };
 
 function streamToDir(urlArray, directory){
-  //sept 7 2016 work from here...-sW 
-  console.log(urlArray);
+  for (var i = 0; i < urlArray.length; i++){
+    request(urlArray[i]).pipe(fs.createWriteStream('./avatars/image' + i +'.jpg'));
+  }
+
 };
 
 
-// fs.stat('./avatars', function(err, stats){
-//   if (stats && !err){
-//     var dataObj = parseGithubData(username, repo, getURLArray);
+fs.stat('./avatars', function(err, stats){
+  if (stats && !err){
 
-//   } else if (!stats && err.errno === -2) {
-//     //make the dir then do business logic
-//     fs.mkdirSync('./avatars');
-//   } else {
-//     //pray to god
-//     console.log(err);
-//   }
-// });
+    parseGithubData(username, repo, getURLArray);
 
-parseGithubData(username, repo, getURLArray);
+  } else if (err.errno === -4058 || err.errno === -2) {
+    //errno seems to changed based on windows(-4058) vs OSX(-2)???
+    
+    fs.mkdirSync('./avatars');
+    parseGithubData(username, repo, getURLArray);
+
+  } else {
+    //pray to god
+    console.log(err);
+  }
+});
